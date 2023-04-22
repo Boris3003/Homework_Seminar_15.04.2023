0b19031e2f0af6769ea427867f1993f014c4235b
Boris <D:\The Bat!>
2023-04-22 23:40:34 +0300

    ex001

---
+++ ex001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверка числа на палиндром
+Console.Write("Введите пятизначное число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int a = N/10000;
+int b = (N/1000)%10;
+int c = (N/10)%10;
+int d = N%10;
+
+if (a == d && b ==c) Console.WriteLine("Это число палиндром");
+else Console.WriteLine("Это число не палиндром");
