87c4734b92f26e52b02265cc594cb22d1a5ab9aa
Boris <D:\The Bat!>
2023-04-23 00:11:06 +0300

    ex003

---
+++ ex003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Таблица кубов чисел от 1 до N
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int cube = 1;
+for (int count = 1; count<=N; count++)
+{
+    cube = count*count*count;
+    Console.WriteLine($"{cube}");
+} 
